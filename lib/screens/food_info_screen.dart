7be160051584c9ded0f5aa8465939386606c6355
Ryan Zander <rjameszander@gmail.com>
2019-06-11 16:00:50 +0700

    food info UI

--- lib/screens/food_info_screen.dart
+++ lib/screens/food_info_screen.dart
@@ -43,167 +43,171 @@ class FoodInfoState extends State<FoodInfoScreen> {
         child: Center(
           child: Column(
             children: <Widget>[
-              Row(
-                children: <Widget>[
-                  Expanded(
-                    child: Container(
-                      padding: EdgeInsets.all(12.0),
-                      child: AutoSizeText(
-                        '${widget.food.nameThai}',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 20,
-                        ),
-                        maxLines: 1,
-                        textAlign: TextAlign.right,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              Container(
-                margin: EdgeInsets.only(left: 40.0, right: 40.0, bottom: 25.0),
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Colors.white,
-                    width: 6,
-                  ),
-                  boxShadow: <BoxShadow>[
-                    BoxShadow(
-                      color: Colors.black,
-                      offset: Offset(2.0, 6.0),
-                      blurRadius: 12.0,
-                    ),
-                  ],
-                ),
-                child: Stack(
-                  children: <Widget>[
-                    Image.asset(
-                      'assets/images/food_images/${widget.food.image}',
-                    ),
-                    Row(
-                      children: <Widget>[
-                        Expanded(
-                          child: Container(
-                            padding: EdgeInsets.all(6.0),
-                            color: Colors.white.withAlpha(225),
-                            child: Column(
-                              children: <Widget>[
-                                Row(
-                                  children: <Widget>[
-                                    Expanded(
-                                      child: AutoSizeText(
-                                        '${widget.food.nameEng}',
-                                        style: TextStyle(
-                                          color: textGreen,
-                                          fontSize: 12,
-                                        ),
-                                        maxLines: 2,
-                                        textAlign: TextAlign.left,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                                Container(
-                                  height: 4.0,
-                                ),
-                                Row(
-                                  children: <Widget>[
-                                    Expanded(
-                                      child: AutoSizeText(
-                                        '${widget.food.nameThaiEng}',
-                                        style: TextStyle(
-                                          color: textRed,
-                                          fontSize: 15,
-                                        ),
-                                        maxLines: 2,
-                                        textAlign: TextAlign.left,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ],
-                            ),
-                          ),
-                        )
-                      ],
-                    )
-                  ],
-                ),
-              ),
-              Container(
-                height: 40.0,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage('assets/images/general/taste_bg.png'),
-                    fit: BoxFit.fill,
-                  ),
-                ),
-                child: Center(
-                  child: Padding(
-                    padding: EdgeInsets.only(top: 6.0, bottom: 6.0),
-                    child: Image.asset(
-                      'assets/images/icons/${widget.food.spiciness}.png',
-                    ),
-                  ),
-                ),
-              ),
-              Row(
-                children: <Widget>[
-                  Expanded(
-                    child: Container(
-                      padding: EdgeInsets.all(12.0),
-                      color: mediumGreen,
-                      child: Text(
-                        '${widget.food.desc}',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 17,
-                        ),
-                        textAlign: TextAlign.center,
-                      ),
-                    ),
-                  )
-                ],
-              ),
-              Expanded(
-                child: Container(),
-              ),
-              Container(
-                padding: EdgeInsets.all(12.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Container(
-                      height: 40.0,
-                      width: 40.0,
-                      child: FlatButton(
-                        padding: EdgeInsets.all(0.0),
-                        child: Image.asset(
-                            'assets/images/icons/button_favorites.png'),
-                        onPressed: () {},
-                      ),
-                    ),
-                    Container(
-                      width: 20.0,
-                    ),
-                    Container(
-                      height: 40.0,
-                      width: 40.0,
-                      child: FlatButton(
-                        padding: EdgeInsets.all(0.0),
-                        child: Image.asset('assets/images/icons/play.png'),
-                        onPressed: () {
-                          playAudio(widget.food.id);
-                        },
-                      ),
-                    )
-                  ],
-                ),
-              ),
+              foodName(),
+              imageBox(),
+              chiliBox(),
+              descBox(),
+              bottomButtons(),
             ],
           ),
         ),
+      ),
+    );
+  }
+
+  //food name
+  Widget foodName() {
+    return Row(
+      children: <Widget>[
+        Expanded(
+          child: Container(
+            padding: EdgeInsets.all(12.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                AutoSizeText(
+                  '${widget.food.nameEng}',
+                  style: TextStyle(
+                    color: lightYellow,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w600,
+                  ),
+                  maxLines: 2,
+                  textAlign: TextAlign.center,
+                ),
+                Container(height: 4),
+                AutoSizeText(
+                  '${widget.food.nameThaiEng}',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 17,
+                    fontWeight: FontWeight.w500,
+                  ),
+                  maxLines: 2,
+                  textAlign: TextAlign.center,
+                ),
+                Container(height: 2),
+                AutoSizeText(
+                  '${widget.food.nameThai}',
+                  style: TextStyle(
+                    color: textGreen,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w500,
+                  ),
+                  maxLines: 2,
+                  textAlign: TextAlign.center,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  // image box
+  Widget imageBox() {
+    return Expanded(
+      child: Center(
+        child: Padding(
+          padding: EdgeInsets.only(bottom: 25.0, left: 40.0, right: 40.0),
+          child: Container(
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Colors.white,
+                width: 4,
+              ),
+              boxShadow: <BoxShadow>[
+                BoxShadow(
+                  color: Colors.black,
+                  offset: Offset(2.0, 6.0),
+                  blurRadius: 12.0,
+                ),
+              ],
+            ),
+            child: Image.asset(
+              'assets/images/food_images/${widget.food.image}',
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  // chili box
+  Widget chiliBox() {
+    return Container(
+      height: 40.0,
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('assets/images/general/taste_bg.png'),
+          fit: BoxFit.fill,
+        ),
+      ),
+      child: Center(
+        child: Padding(
+          padding: EdgeInsets.only(top: 6.0, bottom: 6.0),
+          child: Image.asset(
+            'assets/images/icons/${widget.food.spiciness}.png',
+          ),
+        ),
+      ),
+    );
+  }
+
+  // desc box
+  Widget descBox() {
+    return Row(
+      children: <Widget>[
+        Expanded(
+          child: Container(
+            padding: EdgeInsets.all(12.0),
+            color: mediumGreen,
+            child: Text(
+              '${widget.food.desc}',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 17,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ),
+        )
+      ],
+    );
+  }
+
+  // bottom buttons
+  Widget bottomButtons() {
+    return Container(
+      padding: EdgeInsets.all(12.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Container(
+            height: 40.0,
+            width: 40.0,
+            child: FlatButton(
+              padding: EdgeInsets.all(0.0),
+              child: Image.asset('assets/images/icons/button_favorites.png'),
+              onPressed: () {},
+            ),
+          ),
+          Container(
+            width: 20.0,
+          ),
+          Container(
+            height: 40.0,
+            width: 40.0,
+            child: FlatButton(
+              padding: EdgeInsets.all(0.0),
+              child: Image.asset('assets/images/icons/play.png'),
+              onPressed: () {
+                playAudio(widget.food.id);
+              },
+            ),
+          )
+        ],
       ),
     );
   }
